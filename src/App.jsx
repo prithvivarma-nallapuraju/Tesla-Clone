c51765995b6248b677ffa2afe66cf4eec1bf0c14
prithvivarma-nallapuraju <nvpprithvivarma@gmail.com>
2023-04-12 10:39:17 +0530

    created a skeleton for the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+        <h1 className='text-5xl font-extrabold text-center text-red-600'> Hello</h1>
+    </div>
+  )
+}
+
+export default App
